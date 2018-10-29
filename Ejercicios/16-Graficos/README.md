4bf9e62810979d1113945bad7411334565917ba8
Matias Garcia <matiasagt@gmail.com>
2018-10-29 20:02:18 -0300

    16 - Graficos - Fin de la 16ta tarea del curso

---
+++ Ejercicios/16-Graficos/README.md
@@ -0,0 +1,15 @@
+# Graficos
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Temas
+
+1. Instalaciones necesarias
+2. Configuraciones
+3. Gráfico de Barras
+4. Gráfico de Donas
+5. Gráfico de Radar
---
+++ Ejercicios/16-Graficos/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
